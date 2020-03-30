1c208c9618d04b5d67f6ba1cea4b3cfc738755fc
Eden Shuker <edenshuker1997@gmail.com>
2020-03-30 13:51:29 +0300

    Initial Commit

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='jenkins_proj',
+      version='1.0',
+      description='Example tp project using jenkins',
+      author='Eden Shuker',
+      packages=['distutils', 'distutils.command'],
+      )
---
+++ test/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='jenkins_proj_test',
+      version='1.0',
+      description='Test project example',
+      author='Eden Shuker',
+      packages=['distutils', 'distutils.command'],
+      )
